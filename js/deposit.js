3f672662f439d7e5ed2209aebce09a96b5ce98e1
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-07 17:50:46 +0600

    bugs:deposit.js all bugs fixed

--- js/deposit.js
+++ js/deposit.js
@@ -5,21 +5,24 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     2. get the value from the element
     3. convert string value to a number
     */
-  const newDepositAmount = getInputFieldValueById("deposit-field");
+  const newDepositAmount = document.getElementById("deposit-field").value;
   /* 
     1. get previous deposit total by id
 
     */
-  const previousDepositTotal = getTextElementValueById("deposit-total");
+  const previousDepositTotal = document.getElementById("deposit-total").innerText;
 
   // calculate new deposit total
-  const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  const newDepositTotal = parseInt(previousDepositTotal) + parseInt(newDepositAmount);
+  
   // set deposit total value
-  setTextElementValueById("deposit-total", newDepositTotal);
+  document.getElementById("deposit-total").innerText = newDepositTotal;
+
 
   // get previous balance by using the function
-  const previousBalanceTotal = document.getElementById("balance-total");
-  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
+  const previousBalanceTotal = document.getElementById("balance-total").innerText;
+  const newBalanceTotal = parseInt(previousBalanceTotal)  + parseInt(newDepositAmount);
+
+  document.getElementById("balance-total").innerText = newBalanceTotal;
+
 });
